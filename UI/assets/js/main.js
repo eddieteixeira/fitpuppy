c1b75be7f0c5c037a55940f94605724e3c64528f
eddieteixeira <eddieteixeira.dev@gmail.com>
2020-08-11 14:51:42 -0300

    Trabalhando na home

---
+++ UI/assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+$( document ).ready(function() {
+  $(".slick-slider").slick({
+    slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 5000,
+     arrows:false,
+      // dots: false, Boolean
+     // arrows: false, Boolean
+   });
+   console.log('jquery');
+});
+
+
