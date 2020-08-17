d13a1577cda8b1a881484c97222fcc425e304b8b
eddieteixeira <eddieteixeira.dev@gmail.com>
2020-08-17 16:28:04 -0300

    home finalizada com responsivo

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,15 +1,53 @@
 $( document ).ready(function() {
-  $(".slick-slider").slick({
-    slidesToShow: 1,
-     slidesToScroll: 1,
-     autoplay: true,
-     autoplaySpeed: 5000,
-     arrows:false,
-      // dots: false, Boolean
-     // arrows: false, Boolean
-   });
-   console.log('jquery');
+
+  $('.slick-produtos').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  
+  $('.slick-lojas').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
 });
-
 
